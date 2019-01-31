8232e9e5cd578f23121b42ed941556d0cd645aab
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-31 00:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlays (musicians, instruments) {
+
+var MyArray = [];
+
+for (let i = 0; i < 4; i++) {
+  MyArray.push(`${musicians.i} plays ${instruments.i}`)
+}
+return MyArray;
+}// add solution here
