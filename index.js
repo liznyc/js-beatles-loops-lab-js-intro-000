43d81c8a2224806dfa6b0725a1aacd74d8ab7354
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-31 01:20:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+
+var MyArray = [];
+
+for (let i = 0; i < musicians.length; i++) {
+  MyArray.push(musicians[i] + " plays " + instruments[i])
+}
+return MyArray;
+}// add solution here
+
+function johnLennonFacts (array) {
+let newArray = []
+var n = 0;
+  while (n < array.length) {
+    newArray.push(array[n] +"!!!");
+    n++
+}
+return newArray;
+}
+
+function iLoveTheBeatles (n) {
+var emptyArray = [];
+let i = n;
+do {
+  emptyArray.push("I love the Beatles!")
+  i++;
+} while (i < 15);
+return emptyArray;
+}
