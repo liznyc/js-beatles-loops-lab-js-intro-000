5cc726213b32663b542d6a5f824f854400d59be0
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-31 00:42:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function theBeatlesPlays (musicians, instruments) {
 
 var MyArray = [];
 
-for (let i = 0; i < 4; i++) {
-  MyArray.push(`${[musicians.i]} plays ${[instruments.i]}`)
+for (let i = 0; i < musicians.length; i++) {
+  MyArray.push(musicians[i] + "plays" + instruments[i])
 }
 return MyArray;
 }// add solution here
